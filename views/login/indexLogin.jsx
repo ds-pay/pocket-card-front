d13c0cba4291c0e41ca28ad5bce3729fc21d4274
Anderson Lopez <lopezanderson069@gmail.com>
2022-11-04 17:03:17 -0500

    culminacion de la seccion del login y comienzo de la card login

--- views/login/indexLogin.jsx
+++ views/login/indexLogin.jsx
@@ -1,19 +1,96 @@
-import React from 'react'
-import { Container } from './stylesIndexLogin'
+import React from "react";
+import {
+  ContainerLogin,
+  ContainerCard,
+  ContainerNetworks,
+  Login,
+  ContainerBanner,
+  ContainerTitleBanner,
+  WaveTopBanner,
+  DiamondContainerBanner,
+  WaveBottomBanner,
+} from "./stylesIndexLogin";
+import InputText from "../../components/Inputs/InputText/InputText";
+import {
+  FaUserAlt,
+  FaLock,
+  FaFacebookSquare,
+  FaInstagram,
+} from "react-icons/fa";
 
 const LoginView = () => {
+  const ArrayInputsText = [
+    {
+      name: "ID Pocket Card",
+      color: true,
+      type: "email",
+      img: <FaUserAlt />,
+    },
+    {
+      name: "Password",
+      color: true,
+      type: "password",
+      img: <FaLock />,
+    },
+  ];
+
+  const ArrayNetworks = [
+    {
+      id: "facebook",
+      img: <FaFacebookSquare size={"2rem"} />,
+      alt: "facebook",
+    },
+    {
+      id: "instagram",
+      img: <FaInstagram size={"2rem"} />,
+      alt: "instagram",
+    },
+  ];
+
   return (
     <>
-      <Container>
-        <div className='seccionImg'>
-
-        </div>
-        <div className='seccionLogin'>
-          
-        </div>
-      </Container>
+      <div style={{display: "flex", flexDirection:"row", justifyContent: "center", alignItems: "center"}}>
+        <ContainerCard>
+          <h1>Holis como estas</h1>
+        </ContainerCard>
+        <ContainerLogin>
+          <Login>
+            <div className="img-user">
+              <img src="/img/image/user.png" alt="userImg" />
+            </div>
+            <div className="container-title">
+              <h1>Iniciar Sesion</h1>
+            </div>
+            <div className="seccionLogin">
+              {ArrayInputsText.map((sec, index) => (
+                <div className="container">
+                  <div className="container-img">{sec.img}</div>
+                  <div className="container-text">
+                    <InputText
+                      name={sec.name}
+                      color={sec.color}
+                      type={sec.type}
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="container-button">
+              <button>Login</button>
+            </div>
+          </Login>
+          <ContainerNetworks>
+            <h3 className="text-networks">Nuestras Redes</h3>
+            <div className="networks">
+              {ArrayNetworks.map((sec, index) => (
+                <div className="network">{sec.img}</div>
+              ))}
+            </div>
+          </ContainerNetworks>
+        </ContainerLogin>
+      </div>
     </>
   );
-}
+};
 
-export default LoginView
+export default LoginView;
--- views/login/stylesIndexLogin.jsx
+++ views/login/stylesIndexLogin.jsx
@@ -1,16 +1,138 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.div`
+export const ContainerLogin = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30rem;
+  height: 40rem;
+  background-color: #272727;
+  flex-direction: column;
+  border-radius: 1rem 1rem 1rem 1rem;
+  position: absolute;
+
+  .img-user,
+  .seccionLogin {
     display: flex;
+  }
+
+  .img-user {
+    position: absolute;
+    top: 8rem;
+    background-color: #272727;
+    padding: 1rem;
+    border-radius: 6rem 6rem 6rem 6rem;
+    /* border-bottom: solid white 2px; */
+    img {
+      width: 9rem;
+    }
+  }
+
+  .container-title {
+    display: flex;
+    flex-direction: column;
+    font-family: "Roboto", sans-serif;
+    height: 3rem;
     justify-content: center;
     align-items: center;
-    width: 30rem;
-    height: 40rem;
-    background-color: #272727;
-    flex-direction: column;
-    border-radius: 1rem 1rem 1rem 1rem;
-
-    .seccionImg, .seccionLogin{
-        display: flex;
+    h1 {
+      margin: 0;
     }
+  }
+
+  .seccionLogin {
+    width: 25rem;
+    height: 15rem;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .container {
+      display: flex;
+      margin: 1rem 0 1rem 0;
+      .container-img,
+      .container-text {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 0 0.5rem 0 0.5rem;
+      }
+      .container-img {
+        padding: 1rem;
+        background-color: #5c5c5c;
+        border-radius: 0.2rem;
+      }
+    }
+  }
+  .container-button {
+    width: 70%;
+    height: 2.3rem;
+    button {
+      width: 100%;
+      height: 100%;
+      background-color: #5c5c5c;
+      transition: 0.3s all ease;
+      border: none;
+      border-radius: 0.3rem;
+      :hover {
+        background-color: #272727;
+        cursor: pointer;
+      }
+    }
+  }
 `;
+
+export const Login = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const ContainerNetworks = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+  height: 3rem;
+  margin: 1rem 0 0 0;
+
+  .text-networks {
+    border-top: solid white 1px;
+    width: 180%;
+    text-align: center;
+    font-family: 'Roboto',sans-serif;
+    padding: 10px 0px 0px 0px;
+  }
+  .networks {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    .network {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 0.5rem 1.3rem 0.5rem 1.3rem;
+      background-color: #5c5c5c;
+      padding: 0.5rem;
+      border-radius: 0.5rem;
+      transition: 0.3s all ease;
+      :hover {
+        background-color: #272727;
+        cursor: pointer;
+      }
+    }
+  }
+`;
+
+export const ContainerCard = styled.div`
+  z-index: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30rem;
+  height: 51rem;
+  background-color: #5c5c5c;
+  border-radius: 2rem;
+`;
+
