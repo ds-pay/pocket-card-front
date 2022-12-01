fe994f9f99eec81259969bbbc27b15a8d4e53062
Anderson Lopez <lopezanderson069@gmail.com>
2022-12-01 17:01:26 -0500

    Primer commit para rama crear comercio

--- pages/administrador/cuentas/index.jsx
+++ pages/administrador/cuentas/index.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FormularioCrearCliente from './FormularioCrearCliente'
+import FormularioCrearComercio from './FormularioCrearComercio'
+import styled from '@emotion/styled'
+
+
+const ContentCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+  .cardContent{
+    display: flex;
+    .card{
+      
+    }
+  }
+
+`;
 
 const index = () => {
+  const [ secSelection, setSecSelection ] = useState(false)
+  const [ section, setSection ] = useState("")
+  
+
+
+  const SeleccionSeccion = [
+    {
+      id: "formClient",
+      content: <FormularioCrearCliente />,
+      Label: "Crear Cliente"
+    },
+    {
+      id: "formCommerce",
+      content: <FormularioCrearComercio />,
+      Label: "Crear Comercio"
+    }
+  ]
+
+  const handleCreateSection = () =>{
+    if ( section === "formClient" ){
+      return <FormularioCrearCliente />
+    } else {
+      return <FormularioCrearComercio />
+    }
+  }
+
+
   return (
     <>
-      <FormularioCrearCliente />
+      { secSelection
+      ? <>{handleCreateSection()}</>
+      : <ContentCard>
+          <h1>Elige una opcion</h1>
+          <div className='cardContent'>
+            {SeleccionSeccion.map((sec, index) => (
+              <div className='card'>
+                holis
+              </div>
+            ))}
+          </div>
+        </ContentCard>
+      }
     </>
   )
 }
