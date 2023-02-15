d1248f38e4d9496a1cdf7ffd5a2e7fe269622aa3
Anderson Lopez <lopezanderson069@gmail.com>
2023-02-15 15:44:13 -0500

    completed card of view frequent questions

--- helpers/categoriesData.js
+++ helpers/categoriesData.js
@@ -1,63 +1,63 @@
 const categories = [
   {
     id: "bienestar",
-    icon: "/img/Icons/cat_bienestar.svg",
+    icon: "/img/Icons/categories/cat_bienestar.svg",
     label: "Mi Bienestar",
     text: "Suspendisse posuere scelerisque leo ac varius. Aliquam bibendum eu sem vitae mattis.",
     colorcategory: "#340064c5"
   },
   {
     id: "diversion",
-    icon: "/img/Icons/cat_diversion.svg",
+    icon: "/img/Icons/categories/cat_diversion.svg",
     label: "Vamos a Divertirnos",
     text: "Suspendisse posuere scelerisque leo ac varius. Aliquam bibendum eu sem vitae mattis.",
     colorcategory: "#340064c5"
   },
   {
     id: "estudiar",
-    icon: "/img/Icons/cat_estudiar.svg",
+    icon: "/img/Icons/categories/cat_estudiar.svg",
     label: "Vamos a Estudiar",
     text: "Suspendisse posuere scelerisque leo ac varius. Aliquam bibendum eu sem vitae mattis.",
     colorcategory: "#340064c5"
   },
   {
     id: "hogar",
-    icon: "/img/Icons/cat_hogar.svg",
+    icon: "/img/Icons/categories/cat_hogar.svg",
     label: "Mi Hogar",
     text: "Suspendisse posuere scelerisque leo ac varius. Aliquam bibendum eu sem vitae mattis.",
     colorcategory: "#340064c5"
   },
   {
     id: "mascotas",
-    icon: "/img/Icons/cat_mascotas.svg",
+    icon: "/img/Icons/categories/cat_mascotas.svg",
     label: "Mi Mascota",
     text: "Suspendisse posuere scelerisque leo ac varius. Aliquam bibendum eu sem vitae mattis.",
     colorcategory: "#340064c5"
   },
   {
     id: "seguridad",
-    icon: "/img/Icons/cat_seguridad.svg",
+    icon: "/img/Icons/categories/cat_seguridad.svg",
     label: "Mi Seguridad",
     text: "Suspendisse posuere scelerisque leo ac varius. Aliquam bibendum eu sem vitae mattis.",
     colorcategory: "#340064c5"
   },
   {
     id: "shopping",
-    icon: "/img/Icons/cat_shopping.svg",
+    icon: "/img/Icons/categories/cat_shopping.svg",
     label: "Vamos de Shopping",
     text: "Suspendisse posuere scelerisque leo ac varius. Aliquam bibendum eu sem vitae mattis.",
     colorcategory: "#340064c5"
   },
   {
     id: "transporte",
-    icon: "/img/Icons/cat_transporte.svg",
+    icon: "/img/Icons/categories/cat_transporte.svg",
     label: "Mi Transporte",
     text: "Suspendisse posuere scelerisque leo ac varius. Aliquam bibendum eu sem vitae mattis.",
     colorcategory: "#340064c5"
   },
   {
     id: "viajar",
-    icon: "/img/Icons/cat_viajar.svg",
+    icon: "/img/Icons/categories/cat_viajar.svg",
     label: "Vamos a Viajar",
     text: "Suspendisse posuere scelerisque leo ac varius. Aliquam bibendum eu sem vitae mattis.",
     colorcategory: "#340064c5"
---
+++ views/view-quiestion/Questions.jsx
@@ -0,0 +1,27 @@
+import { ContainerGeneral, BodyHeader, ContentQuestions } from './StyleQuestions'
+
+const Questions = ({ data }) => {
+  return (
+    <ContainerGeneral>
+      <BodyHeader>
+        <div className="container-title1">
+          <h2><strong>Preguntas Frecuentes</strong></h2>
+        </div>
+        <div className="container-img">
+          <img src="/img/Icons/quiestion/pregunta.svg" alt={`img-bonus`} />
+        </div>
+      </BodyHeader>
+      {
+        data && 
+        data.map((sec, index) => (
+          <ContentQuestions key={index}>
+            <h2><strong>{parseInt(index += 1)}. {sec.label}</strong></h2>
+            <p>{sec.text}</p>
+          </ContentQuestions>
+        ))
+      }
+    </ContainerGeneral>
+  )
+}
+
+export default Questions
