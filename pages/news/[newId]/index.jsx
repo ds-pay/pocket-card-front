6f9b21ca08feb88ec4bb7eca8f8129109bf2a90a
Anderson Lopez <lopezanderson069@gmail.com>
2023-02-14 21:35:26 -0500

    solution of error paths name

--- pages/news/[newId]/index.jsx
+++ pages/news/[newId]/index.jsx
@@ -12,15 +12,15 @@ export async function getStaticPaths() {
   const res = await fetch(url);
   const json = await res.json()
   const paths = await json.news.map((notice) => ({
-    params: { notice: notice.id}
+    params: { newId: notice.id}
   }))
 
   return { paths, fallback:false }
 };
 
 export async function getStaticProps({ params }) {
-  const { notice } = params;
-  const url = `${process.env.API_LOCAL}/api/news/${notice}`;
+  const { newId } = params;
+  const url = `${process.env.API_LOCAL}/api/news/${newId}`;
   const res = await fetch(url);
   const json = await res.json()
   console.log(json)
