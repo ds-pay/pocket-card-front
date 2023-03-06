b79be6e828f75383b25d63f31da43094065c2302
Anderson Lopez <lopezanderson069@gmail.com>
2023-03-06 17:03:55 -0500

    social network line and apps to download

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import { ContainerFooter } from './StylesFooter'
-import { FaHeadset, FaNewspaper } from 'react-icons/fa'
-import { BsFillQuestionCircleFill } from 'react-icons/bs'
-import { useRouter } from 'next/router'
+import { ContainerFooter, RedSocial, ContentSocial, ButtonUrl, ContentBottom, ContainerApp } from './StylesFooter';
+import { FaHeadset, FaNewspaper } from 'react-icons/fa';
+import { BsFillQuestionCircleFill } from 'react-icons/bs';
+import { useRouter } from 'next/router';
+import { AiFillFacebook, AiFillInstagram, AiOutlineWhatsApp, AiFillAndroid, AiFillApple } from 'react-icons/ai'
+import { FcHeadset, FcNews, FcInfo } from 'react-icons/fc'
+
 
 const Footer = () => {
 	const router = useRouter();
@@ -10,12 +13,45 @@ const Footer = () => {
 		router.push(route)
 	}
 
+	const arrayIconRedSocial = [
+		{
+			id: "facebook",
+			icon: <AiFillFacebook/>,
+			url: "",
+		},
+		{
+			id: "instagram",
+			icon: <AiFillInstagram/>,
+			url: "",
+		},
+		{
+			id: "whatsapp",
+			icon: <AiOutlineWhatsApp/>,
+			url: "",
+		},
+	];
+
+	const arrayApps = [
+		{
+			id: "android",
+			icon: <AiFillAndroid />,
+			label: "Decarga para Android",
+			url: "",
+		},
+		{
+			id: "apple",
+			icon: <AiFillApple />,
+			label: "Decarga para IOS",
+			url: "",
+		},
+	]
+
 	const ArrayItems = [
 		{
 			id: "button1",
 			className: "sectionFooter",
 			label: "Habla con nosotros",
-			icon: <FaHeadset />,
+			icon: <FcHeadset />,
 			labelbutton: "Servicio al cliente",
 			route: "/contactus"
 		},
@@ -23,31 +59,54 @@ const Footer = () => {
 			id: "button2",
 			className: "sectionFooter",
 			label: "Conoce lo nuevo",
-			icon: <FaNewspaper />,
-			labelbutton: "Noticias de Viajes",
+			icon: <FcNews />,
+			labelbutton: "Noticias Pocket",
 			route: "/news"
 		},
 		{
 			id: "button3",
 			className: "sectionFooter",
 			label: "Resuelve tus dudas",
-			icon: <BsFillQuestionCircleFill />,
+			icon: <FcInfo />,
 			labelbutton: "Preguntas Frecuentes",
 			route: "/questions"
 		}
-	]
+	];
 
 	return (
 		<ContainerFooter>
-			<div className='divFooterPrimary'>
-				{ArrayItems.map((sec, index) => (
-					<div key={index} className={sec.className}>
-						<h1>{sec.label}</h1>
-						<button onClick={() => handleRouter(sec.route)}>{sec.icon} {sec.labelbutton}</button>
-					</div>
-				))}
-			</div>
-			<div className='divFooterSecundary'> <h1>Pocket Card - Todos los derechos reservados © {new Date().getFullYear()}</h1></div>
+			<ContentBottom>
+				<ButtonUrl>
+					{ArrayItems.map((sec, index) => (
+						<div key={index} onClick={() => handleRouter(sec.route)} className={sec.className}>
+							{/* <h1>{sec.label}</h1> */}
+							{/* <button > */}
+							<h2>{sec.icon}</h2>
+							<h3><strong>{sec.labelbutton}</strong></h3>
+							{/* </button> */}
+						</div>
+					))}
+				</ButtonUrl>
+			</ContentBottom>
+				<div className='divFooterSecundary'> <h1>Pocket Card - Todos los derechos reservados © {new Date().getFullYear()}</h1></div>
+			<ContentSocial>
+				<div className='line-whidth'></div>
+				<div className='container-social'>
+					{arrayIconRedSocial.map((sec, index) => (
+						<RedSocial key={index}>
+							<h2>{sec.icon}</h2>
+						</RedSocial>
+					))}
+				</div>
+				{
+					arrayApps.map((sec, index) => (
+					<ContainerApp key={index}>
+						<h2>{sec.icon}</h2>
+						<h3>{sec.label}</h3>
+					</ContainerApp>
+					))
+				}
+			</ContentSocial>
 		</ContainerFooter>
 	)
 }
--- components/Footer/StylesFooter.jsx
+++ components/Footer/StylesFooter.jsx
@@ -4,50 +4,15 @@ export const ContainerFooter = styled.div`
   display: flex;
   z-index: 100;
   width: 100%;
-  height: 20rem;
-  background: linear-gradient(180deg, var(--nextui-colors-backgroundNavbar) , var(--nextui-colors-backGradiente2) );
+  padding: 2rem 0 0.5rem 0;
+  transition: 0.3s;
+  /* background: linear-gradient(180deg, var(--nextui-colors-backgroundNavbar) , var(--nextui-colors-backGradiente2) ); */
+  background-color: var(--nextui-colors-backgroundNavbar);
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  .divFooterPrimary{
-    height: 80%;
-    width:90%;
-    max-width: 1600px;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    .sectionFooter{
-      width: 30%;
-      height: 90%;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      h1{
-        font-size: 1.2rem;
-        color: white;
-        margin: 2rem 0 2rem 0;
-      }
-      button{
-        width: 16rem;
-        height: 3rem;
-        border-radius: 1rem;
-        background-color: var(--nextui-colors-bgCardLogin);
-        border: none;
-        color: white;
-        transition: all 0.5s;
-        font-size: 1.2rem;
-        padding: 0.2rem 0 0.2rem 0 ;
-        :hover{
-          cursor: pointer;
-          background-color: white;
-          color: black;
-          transform: scale(1.1, 1.1);
-        }
-      }
-    }
-  }
   .divFooterSecundary{
+    margin: 1rem 0;
     height: 30%;
     width:90%;
     max-width: 1600px;
@@ -89,3 +54,161 @@ export const ContainerFooter = styled.div`
     }
   }
 `
+
+export const RedSocial = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 1rem;
+  width: 4rem;
+  background-color: var(--nextui-colors-backgroundNavbar);
+  h2{
+    transition: all ease 0.3s;
+    margin: 1rem 2rem;
+    color: #ededed;
+    margin: 0;
+    :hover{
+      transform: scale(1.2);
+      cursor: pointer;
+    }
+  }
+  @media(max-width: 1430px){
+    width: 2.5rem;
+  }
+`;
+
+export const ContentSocial = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 4rem;  
+  justify-content: space-around;
+  align-items: center;
+  .line-whidth{
+    position: absolute;
+    width: 100%;
+    height: 0.1rem;
+    background-color: white;
+    border-radius: 5rem 6rem 6rem 0rem;
+  }
+  .container-social{
+    z-index: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 2rem;
+    border-radius: 0.5rem;
+    transition: 0.3s;
+    @media(max-width: 1430px){
+      padding: 0 0 0 0;
+    } 
+  }
+`;
+
+export const ButtonUrl = styled.div `
+  height: 80%;
+  width:90%;
+  max-width: 1600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .sectionFooter{
+    margin-top: 1rem;
+    margin:  0.5rem 2rem;
+    padding: 0 0.5rem;
+    width: 15rem;
+    height: 4rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    background-color: white;
+    border-radius: 1rem;
+    -webkit-box-shadow: 0px 0px 7px 0px rgba(149, 149, 149, 0.591);
+    -moz-box-shadow: 0px 0px 7px 0px rgba(149, 149, 149, 0.591);
+    box-shadow: 0px 5px 5px 0px rgba(149, 149, 149, 0.591);
+    transition: all ease 0.3s;
+    h2{
+      font-size: 3rem;
+      margin: 0;
+    }
+    h3{
+      font-size: 0.9rem;
+      margin: 0;
+    }
+    :hover{
+      cursor: pointer;
+      transform: scale(1.1);
+    }
+
+      /* h1{
+        font-size: 1.2rem;
+        color: white;
+        margin: 2rem 0 2rem 0;
+      } */
+      /* button{
+        width: 16rem;
+        height: 3rem;
+        border-radius: 1rem;
+        background-color: var(--nextui-colors-bgCardLogin);
+        border: none;
+        color: white;
+        transition: all 0.5s;
+        font-size: 1.2rem;
+        padding: 0.2rem 0 0.2rem 0 ;
+        :hover{
+          cursor: pointer;
+          background-color: white;
+          color: black;
+          transform: scale(1.1, 1.1);
+        }
+      } */
+    }
+    @media (max-width: 1000px){
+      flex-direction: column;
+      justify-content: space-evenly;
+      align-items: center;
+      .sectionFooter{
+        margin: 0.5rem 0;
+        padding: 0 0.2rem;
+        width: 12rem;
+        height: 5rem;
+        flex-direction: row;
+        justify-content: space-around;
+        h2{
+          font-size: 2.5rem;
+        }
+        h3{
+          font-size: 0.8rem;
+        }
+      }
+    }
+`;
+
+export const ContentBottom = styled.div`
+  display: flex;
+`;
+
+export const ContainerApp = styled.div`
+  display: flex;
+  width: 20%;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  color: white;
+  transition: 0.3s;
+  background-color: var(--nextui-colors-backgroundNavbar);
+  h2{
+    margin: 0;
+  }
+  h3{
+    transition: 0.3s;
+    margin: 0 0.5rem;
+  }
+  @media(max-width: 1430px){
+    width: auto;
+    h3{
+      display: none;
+    }
+  } 
+`;
